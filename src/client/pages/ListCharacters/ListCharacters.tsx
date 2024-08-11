b4bfcf25ff8b4c2c584651d4c3060b1044d9d5a1
Omer Dolev <53278705+dolevoper@users.noreply.github.com>
2024-08-11 13:20:43 +0000

    add loader

--- src/client/pages/ListCharacters/ListCharacters.tsx
+++ src/client/pages/ListCharacters/ListCharacters.tsx
@@ -1,88 +1,108 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useState } from "react";
+import { Link, Outlet, useLoaderData, useSearchParams } from "react-router-dom";
 import Input from "../../components/Input";
 import Icon from "../../components/Icon";
 import Button from "../../components/Button";
 import styles from "./ListCharacters.module.css";
 
 import type { Character } from "../../../server/db/schema";
-import { Link, Outlet } from "react-router-dom";
 
 export default function ListCharacters() {
-  const [name, setName] = useState("");
-  const [page, setPage] = useState(1);
-  const [data, setData] = useState<{ data: Character[], page: number, pageCount: number }>();
-
-  useEffect(() => {
-    async function getCharacters() {
-      const res = await axios.get("/characters", { params: { name, page } });
-
-      setData(res.data);
-    }
-
-    getCharacters();
-  }, [name, page]);
-
   return (
     <>
+      <Header />
+      <Search />
+      <Pagination />
+      <CharacterList />
       <Outlet />
+    </>
+  );
+}
+
+function Header() {
+  return (
+    <>
       <h1>Characters</h1>
       <menu className="cluster">
         <li>
           <Link to="create">New character</Link>
         </li>
       </menu>
-      <form>
-        <Input
-          id="character-search"
-          type="search"
-          label="Search"
-          value={name}
-          onInput={(e) => setName(e.currentTarget.value)}
-        />
-      </form>
-      <menu className="cluster center">
-        <li>
-          <Button onClick={() => setPage(page - 1)}><Icon name="chevronLeft" /></Button>
-        </li>
-        <li>
-          Showing page {page} of {data?.pageCount}
-        </li>
-        <li>
-          <Button onClick={() => setPage(page + 1)}><Icon name="chevronRight" /></Button>
-        </li>
-      </menu>
-      {!data?.data.length ? <p>No characters to display.</p> : (<ul>
-        {data.data.map((character) => (
-          <li key={character.id} className={`stack ${styles.characterCard}`}>
-            <h2>{character.name}</h2>
-            <p className={styles.className}>{character.class}</p>
-            <p className={styles.bio}>{character.bio}</p>
-            <ul className="cluster">
-              <li className={styles.stat} title="Strength">
-                <span>STR</span>
-                <span>{character.strength}</span>
-              </li>
-              <li className={styles.stat} title="Dexterity">
-                <span>DEX</span>
-                <span>{character.dexterity}</span>
-              </li>
-              <li className={styles.stat} title="Agility">
-                <span>AGL</span>
-                <span>{character.agility}</span>
-              </li>
-              <li className={styles.stat} title="Intelligence">
-                <span>INT</span>
-                <span>{character.intelligence}</span>
-              </li>
-              <li className={styles.stat} title="Charisma">
-                <span>CHR</span>
-                <span>{character.charisma}</span>
-              </li>
-            </ul>
-          </li>
-        ))}
-      </ul>)}
     </>
   );
+}
+
+function Search() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  return (
+    <form>
+      <Input
+        id="character-search"
+        type="search"
+        label="Search"
+        onInput={(e) => {
+          const newSearchParams = new URLSearchParams(searchParams);
+
+          newSearchParams.set("name", e.currentTarget.value);
+          setSearchParams(newSearchParams);
+        }}
+      />
+    </form>
+  );
+}
+
+function Pagination() {
+  const [page, setPage] = useState(1);
+  const pageCount = 10;
+
+  return (
+    <menu className="cluster center">
+      <li>
+        <Button onClick={() => setPage(page - 1)}><Icon name="chevronLeft" /></Button>
+      </li>
+      <li>
+        Showing page {page} of {pageCount}
+      </li>
+      <li>
+        <Button onClick={() => setPage(page + 1)}><Icon name="chevronRight" /></Button>
+      </li>
+    </menu>
+  );
+}
+
+function CharacterList() {
+  const data = useLoaderData() as { data: Character[], page: number, pageCount: number };
+
+  return !data?.data.length ? <p>No characters to display.</p> : (<ul>
+    {data.data.map((character) => (
+      <li key={character.id} className={`stack ${styles.characterCard}`}>
+        <h2>{character.name}</h2>
+        <p className={styles.className}>{character.class}</p>
+        <p className={styles.bio}>{character.bio}</p>
+        <ul className="cluster">
+          <li className={styles.stat} title="Strength">
+            <span>STR</span>
+            <span>{character.strength}</span>
+          </li>
+          <li className={styles.stat} title="Dexterity">
+            <span>DEX</span>
+            <span>{character.dexterity}</span>
+          </li>
+          <li className={styles.stat} title="Agility">
+            <span>AGL</span>
+            <span>{character.agility}</span>
+          </li>
+          <li className={styles.stat} title="Intelligence">
+            <span>INT</span>
+            <span>{character.intelligence}</span>
+          </li>
+          <li className={styles.stat} title="Charisma">
+            <span>CHR</span>
+            <span>{character.charisma}</span>
+          </li>
+        </ul>
+      </li>
+    ))}
+  </ul>);
 }
--- src/client/router.ts
+++ src/client/router.ts
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import axios from "axios";
 import ListCharacters from "./pages/ListCharacters/ListCharacters";
 import CreateCharacter from "./pages/CreateCharacter/CreateCharacter";
 
@@ -6,6 +7,15 @@ export const router = createBrowserRouter([
     {
         path: "/",
         Component: ListCharacters,
+        async loader({ request }) {
+            const url = new URL(request.url);
+            const name = url.searchParams.get("name");
+            const page = 1;
+
+            const { data } = await axios.get("/characters", { params: { name, page } });
+
+            return data;
+        },
         children: [
             { path: "create", Component: CreateCharacter }
         ]
